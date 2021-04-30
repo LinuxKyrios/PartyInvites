d77138cadb15e82a4d8878787c5f874427160e42
Linux Kyrios <malinowskim92@protonmail.com>
2021-04-30 23:40:55 +0200

    first commit

---
+++ PartyInvites.Tests/Controllers/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace PartyInvites.Tests.Controllers
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestCase()
+        {
+        }
+    }
+}
