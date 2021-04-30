f70cfa34f0158984cc4fd9f2eb89df8b8da2ecf8
Linux Kyrios <malinowskim92@protonmail.com>
2021-04-30 23:56:30 +0200

    Created simple 'Hello world' to check if everything is working properly

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -8,9 +8,9 @@ namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public string Index()
         {
-            return View ();
+            return "Hello Mono/ASP.NET World";
         }
     }
 }
