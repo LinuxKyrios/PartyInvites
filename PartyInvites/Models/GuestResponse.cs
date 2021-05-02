0b7580be6d7b5d2467868c65efc9ac07a106ae50
Linux Kyrios <malinowskim92@protonmail.com>
2021-05-02 21:42:03 +0200

    Added correctness check in GuestResponse.cs file

--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -1,11 +1,22 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+        [Required(ErrorMessage = "Please input your name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please input your email address")]
+        [RegularExpression(".+\\@.+\\..",
+            ErrorMessage = "Please input a correct email address")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "please input your phone number.")]
         public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please choose if you will take part in our party")]
         public bool? WillAttend { get; set; }
     }
     /*This model is for get personal data from invited users*/
